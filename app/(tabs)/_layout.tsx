372445329d48766c627c514c088c974ec3a93983
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 03:47:37 -0500

    Auto-save: 2025-09-14 03:47:37

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,7 +14,6 @@ export default function TabLayout() {
                 name="home"  // internal key, can be anything unique
                 options={{
                     title: 'Home',
-                    href: '/',  // this points to app/index.tsx
                     tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
                 }}
             />
