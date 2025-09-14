66ccc1e3c4b416c4fe7bef8387ed8e9bf7913125
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 15:23:55 -0500

    Auto-save: 2025-09-14 15:23:55

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -30,7 +30,7 @@ export default function RoadmapScreen() {
         <ImageBackground
             source={path}
             style={{ flex: 1, }}
-            imageRef="cover"
+            imaresizeMode="cover"
         >
             <SafeAreaView style={{ flex: 1 }}>
                 <View style={{ flex: 1 }}>
