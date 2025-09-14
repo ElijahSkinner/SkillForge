5b5311ba4ac0cada4cfffe4643f60e26c6e43d49
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 12:07:12 -0500

    Auto-save: 2025-09-14 12:07:12

--- components/CoursesDropdown.tsx
+++ components/CoursesDropdown.tsx
@@ -33,7 +33,7 @@ export default function CoursesDropdown({ onClose, enrolledCourses = [] }: Cours
                     <Pressable
                         style={styles.addCourse}
                         onPress={() => {
-                            router.push('/tabs/course');
+                            router.push('/course');
                             onClose(); // close the dropdown after navigation
                         }}
                     >
