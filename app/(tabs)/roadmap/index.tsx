3085eef3d4a000b87d93e6542befeed0755f9caa
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 14:05:43 -0500

    Auto-save: 2025-09-14 14:05:43

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -23,7 +23,7 @@ export default function RoadmapScreen() {
     }));
 
     return (
-        <ImageBackground source={roadmapBg} style={{ flex: 1 }} resizeMode="contain" >
+        <ImageBackground source={path} style={{ flex: 1 }} resizeMode="contain" >
         <View style={{ flex: 1 }}>
             <TopBar currentStreak={123} currency={456} selectedCourse={{ id: 0, name: selectedCert }} enrolledCourses={enrolledCourses} />
             <QuizRoadmap />
