28bef0255ae45ee4779fd7b8619572934d0b8b69
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 18:37:00 -0500

    Auto-save: 2025-09-14 18:37:00

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -189,7 +189,7 @@ export default function RoadmapScreen() {
                         </View>
                     );
                 })()}
-
+                
             </SafeAreaView>
         </ImageBackground>
     );
