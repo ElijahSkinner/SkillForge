1876653c04ab8950c846fc3c491268a8504d8373
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 04:10:45 -0500

    Auto-save: 2025-09-14 04:10:45

--- app/(tabs)/course/index.tsx
+++ app/(tabs)/course/index.tsx
@@ -1,7 +1,8 @@
 import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { useRouter } from 'expo-router';
-import { useCert } from '@/context/CertContext';
-import { CERTS_ROADMAP } from '@/constants/certs';
+import { useCert } from '../../context/CertContext';
+import { CERTS_ROADMAP } from '../../constants/certs';
+
 
 export default function CourseScreen() {
     const router = useRouter();
