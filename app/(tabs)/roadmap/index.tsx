2470580d4171edb9a84f42df935bd9d1a907742f
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 14:57:42 -0500

    Auto-save: 2025-09-14 14:57:42

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -30,7 +30,7 @@ export default function RoadmapScreen() {
             <View style={{ flex: 1 }}>
             <TopBar currentStreak={123} currency={456} selectedCourse={{ id: 0, name: selectedCert }} enrolledCourses={enrolledCourses} />
             <QuizRoadmap />
-            <ScrollView contantContainerstyle={styles.container}>
+            <ScrollView contantContainerStyle={styles.container}>
                 {modules.map((mod) => (
                     <View key={mod.id} style={styles.section}>
                         <Text style={styles.sectionTitle}>{mod.name}</Text>
