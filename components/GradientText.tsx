020f67d5dcaeefa949df4768efc6168b769489ba
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 17:21:01 -0500

    Auto-save: 2025-09-14 17:21:01

--- components/GradientText.tsx
+++ components/GradientText.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Text, TextProps, StyleSheet } from 'react-native';
 import MaskedView from '@react-native-masked-view/masked-view';
-import LinearGradient from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
 
 type GradientTextProps = TextProps & {
     colors?: string[]; // normal string array
