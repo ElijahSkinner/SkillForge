0efac17bcfe69702237d3ffbe79f427da5c47d47
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 18:32:08 -0500

    Auto-save: 2025-09-14 18:32:08

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -156,7 +156,7 @@ export default function RoadmapScreen() {
                 {selectedLesson && (() => {
                     const mod = modules.find(m => m.id === selectedLesson.modId)!;
                     const lessonCount = mod.lessons.length;
-                    const lessonNum = selectedLesson.lessonIndex + 1;
+                    const lessonNum = selectedLesson.lessonIndex;
                     const xp = getLessonXP(mod, selectedLesson.lessonIndex);
 
                     return (
