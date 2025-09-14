4392a99bad8eec79dbf13875a295fc9cdf3b8a50
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 10:35:07 -0500

    Auto-save: 2025-09-14 10:35:07

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -58,7 +58,7 @@ export default function TabLayout() {
                 }}
             />
             <Tabs.Screen
-                name="quiz/[cert]/[id"
+                name="quiz/[cert]/[id]"
                 options={{
                     href: null,
                 }}
