cb5650246d0f7033d0b426831c35d7c0eefa4eeb
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 17:55:45 -0500

    Auto-save: 2025-09-14 17:55:45

--- types/certs.ts
+++ types/certs.ts
@@ -1,7 +1,13 @@
 // types/certs.ts
+export type LessonType = {
+    id: number;
+    name: string;
+};
+
 export type ModuleType = {
     id: number;
     name: string;
     weight: number;
     completed: boolean;
+    lessons: LessonType[];
 };
