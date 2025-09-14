e3807fa58742ca52bf029c03f4e57eb19d4bf5d4
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 13:32:30 -0500

    Auto-save: 2025-09-14 13:32:30

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {View, ScrollView, StyleSheet, Text, Pressable} from 'react-native';
 import TopBar from '@/components/TopBar';
 import { CERTS_ROADMAP } from '@/constants/certs';
-import { useCert } from '@/context/CertContext';
+//import { useCert } from '@/context/CertContext';
 import { useRouter } from 'expo-router';
 import QuizRoadmap from '@/components/QuizRoadmap';
 export default function RoadmapScreen() {
