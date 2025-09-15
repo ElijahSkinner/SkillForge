a1076022610d7c380cc8e73140ed8d5075dc5bbd
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 21:13:23 -0500

    Auto-save: 2025-09-14 21:13:23

--- constants/glossary.ts
+++ constants/glossary.ts
@@ -14,7 +14,7 @@ export const GLOSSARY_PORTS: Record<string, { port: string; definition: string }
         { port: '389', definition: 'LDAP – Lightweight Directory Protocol' },
         { port: '445', definition: 'SMB - Server Message Block' },
         { port: '514', definition: 'Syslog' },
-        { port: '587', definition: 'SMTPS - Simple Mail Transfer Protocol' },
+        { port: '587', definition: 'SMTPS - Simple Mail Transfer Protocol Secure' },
         { port: '636', definition: 'FTP – File Transfer Protocol' },
         { port: '1433', definition: 'SSH – Secure Shell' },
         { port: '3389', definition: 'SMTP – Simple Mail Transfer Protocol' },
