102992b62daa50fcd17de76aefd01a508c19841f
elijah <elijah.skinner66@gmail.com>
2025-09-15 16:44:03 -0500

    Auto-save: 2025-09-15 16:44:03

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,8 +15,7 @@ export default function RootLayout() {
     return (
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <CertProvider>
-
-
+                <AuthProvider></AuthProvider>
                 <Stack>
                     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                     <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
