d3df59b97cc9f1415fcd3da753c7b4e7b9b9af6d
elijah <elijah.skinner66@gmail.com>
2025-09-15 16:43:47 -0500

    Auto-save: 2025-09-15 16:43:47

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { CertProvider } from '@/context/CertContext'; // <-- import here
-
 import { AuthProvider } from "@/context/AuthContext";
 
 export const unstable_settings = {
