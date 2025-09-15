4e220b4aaea84a86b6e77abcf4155b99ed0d309f
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 19:04:50 -0500

    Auto-save: 2025-09-14 19:04:50

--- constants/certs.ts
+++ constants/certs.ts
@@ -9,7 +9,7 @@ export const CERTS_ROADMAP: Record<string, ModuleType[]> = {
             completed: false,
             lessons: [
                 { id: 1, name: 'OSI Model' },
-                { id: 2, name: 'Networking Appliances and Functions' },
+                { id: 2, name: 'Networking Appliances, Applications, and Functions' },
                 { id: 3, name: 'Cloud Concepts and Connectivity' },
                 { id: 4, name: 'Ports, Protocols, and Traffic' },
                 { id: 5, name: 'Transmission Media and Transceivers' },
