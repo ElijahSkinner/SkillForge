38ada558de0a66079edae1344944f30703aa47e6
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 20:04:07 -0500

    Auto-save: 2025-09-14 20:04:07

--- app/(tabs)/glossary/index.tsx
+++ app/(tabs)/glossary/index.tsx
@@ -67,7 +67,7 @@ export default function GlossaryScreen() {
                     onValueChange={(itemValue: 'Unit 1' | 'Unit 2' | 'Unit 3') => setTab(itemValue)}
                     style={{ color: '#fff', backgroundColor: '#1f1f1f', borderRadius: 8 }}
                 >
-                    <Picker.Item label="Terms" value="terms" />
+                    <Picker.Item label="Unit 1" value="terms" />
                     <Picker.Item label="Acronyms" value="acronyms" />
                     <Picker.Item label="Ports" value="ports" />
                 </Picker>
