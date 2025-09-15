35c7e1320f8fdc1e871d6f4b56f0510afe7dac8a
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-14 19:58:04 -0500

    Auto-save: 2025-09-14 19:58:04

--- app/(tabs)/glossary/index.tsx
+++ app/(tabs)/glossary/index.tsx
@@ -1,4 +1,5 @@
 // app/(tabs)/glossary/index.tsx
+
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, Pressable, Picker } from 'react-native';
 import { useCert } from '@/context/CertContext';
