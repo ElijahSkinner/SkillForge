6a2d8a04a0c4908e3723a4c88359d7cf3852fdf0
elijah <elijah.skinner66@gmail.com>
2025-09-16 10:05:43 -0500

    Auto-save: 2025-09-16 10:05:43

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Pressable, StyleSheet, ActivityIndicator } from "react-native";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "@/context/AuthContext";
 import { router } from "expo-router";
 
 export default function Login() {
