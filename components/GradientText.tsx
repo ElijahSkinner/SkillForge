1b9de88cac09ec153dd212ef4a8ad867d8f34dd0
elijah <elijah.skinner66@gmail.com>
2025-09-16 10:55:39 -0500

    Auto-save: 2025-09-16 10:55:39

--- components/GradientText.tsx
+++ components/GradientText.tsx
@@ -10,7 +10,7 @@ type GradientTextProps = TextProps & {
 
 export default function GradientText({
                                          children,
-                                         colors = ['#222222', ],
+                                         colors = ['#222222', '#fee37f'],
                                          style,
                                          ...props
                                      }: GradientTextProps) {
