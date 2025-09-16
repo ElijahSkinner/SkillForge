ab1b2e44aede04bd227d80cc46b5e09e4fb3a22e
elijah <elijah.skinner66@gmail.com>
2025-09-16 11:41:07 -0500

    Auto-save: 2025-09-16 11:41:07

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -15,7 +15,7 @@ import { useRouter } from 'expo-router';
 import { CERTS_ROADMAP } from '@/constants/certs';
 import {ModuleType} from "@/types/certs";
 import {ImageBackground} from "expo-image";
-import path from "@/assets/images/new-logo.png"
+import path from "@/assets/images/new-logo.png";
 const { TILE_SIZE, TILE_SPACING } = { TILE_SIZE: 60, TILE_SPACING: 8 };
 
 export default function RoadmapScreen() {
