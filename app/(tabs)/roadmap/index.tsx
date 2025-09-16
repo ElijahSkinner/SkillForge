6c79b85a4067d755f958bfb365f096512b0f1741
elijah <elijah.skinner66@gmail.com>
2025-09-16 11:26:16 -0500

    Auto-save: 2025-09-16 11:26:16

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -217,7 +217,7 @@ export default function RoadmapScreen() {
                         </View>
                     </View>
                 );
-            
+
             })()}
         </SafeAreaView>
     );
