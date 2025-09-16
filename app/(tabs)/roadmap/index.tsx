9253d9b9b338f00e8fb63469f6457fed27b0cb6b
elijah <elijah.skinner66@gmail.com>
2025-09-16 10:55:09 -0500

    Auto-save: 2025-09-16 10:55:09

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -165,7 +165,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 12,
         marginBottom: 10,
-        color: '#fee37f',
+        //color: '#fee37f',
     },
     tileText: { color: '#fff', fontWeight: '600' },
     container: { flex: 1, backgroundColor: '#121212', justifyContent: 'center', alignItems: 'center', padding: 16 },
