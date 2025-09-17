f4c46d394bc7b69fea2cf9ac41178f9fe947abab
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-16 20:21:19 -0500

    Auto-save: 2025-09-16 20:21:19

--- app/(tabs)/roadmap/index.tsx
+++ app/(tabs)/roadmap/index.tsx
@@ -14,7 +14,6 @@ import { useCert } from '@/context/CertContext';
 import { useRouter } from 'expo-router';
 import { CERTS_ROADMAP } from '@/constants/certs';
 import {ModuleType} from "@/types/certs";
-//import {  } from "react-native";
 import path from "@/assets/images/path.png";
 const { TILE_SIZE, TILE_SPACING } = { TILE_SIZE: 60, TILE_SPACING: 8 };
 
