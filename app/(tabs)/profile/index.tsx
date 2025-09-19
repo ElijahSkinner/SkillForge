1a687e7ce477583ae356ccbaf96ce4e3497dfa5b
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-19 12:07:30 -0500

    Auto-save: 2025-09-19 12:07:30

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -102,7 +102,7 @@ export default function ProfileScreen() {
                     padding: theme.spacing.md,
                     marginBottom: theme.spacing.md,
                 }}>
-                    <Text style={{ color: theme.colors.textSecondary, fontWeight: "600" }}>Friends</Text>
+                    <Text style={{ color: theme.colors, fontWeight: "600" }}>Friends</Text>
                     <Text style= {{ color: theme.colors.textSecondary}}> No Friends Added Yet :(</Text>
 
                 </View>
