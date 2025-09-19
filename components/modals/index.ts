b56e933d863b1c936760a2eb544df7203d1e27cc
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-19 14:57:36 -0500

    Auto-save: 2025-09-19 14:57:36

---
+++ components/modals/index.ts
@@ -0,0 +1,6 @@
+// components/modals/index.ts - Export file for easy imports
+export { ThemeSelectionModal } from './ThemeSelectionModal';
+export { AccountInfoModal } from './AccountInfoModal';
+export { ChangeEmailModal } from './ChangeEmailModal';
+export { ChangePasswordModal } from './ChangePasswordModal';
+export { LogoutConfirmModal } from './LogoutConfirmModal';
