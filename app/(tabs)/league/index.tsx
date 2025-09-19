86052028bb348987a7f977a3702a5a71cb158d18
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-19 12:45:54 -0500

    Auto-save: 2025-09-19 12:45:54

--- app/(tabs)/league/index.tsx
+++ app/(tabs)/league/index.tsx
@@ -28,6 +28,7 @@ const MOCK_USERS = [
 
 
 export default function LeagueScreen() {
+    
     return (
 <SafeAreaView style={{ flex: 1}}>
     <ScrollView style={styles.container} contentContainerStyle={{ paddingBottom: 30 }}>
