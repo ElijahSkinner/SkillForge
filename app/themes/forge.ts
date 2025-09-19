0f1de68458b981aa5dacabfc69bf2a9a79db0fec
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-18 19:50:26 -0500

    Auto-save: 2025-09-18 19:50:26

---
+++ app/themes/forge.ts
@@ -0,0 +1,13 @@
+// theme/forge.ts
+export const forgeTheme = {
+    colors: {
+        background: "#111",
+        primary: "#f5a623",
+        secondary: "#ddd",
+        text: "#fff",
+    },
+    assets: {
+        logo: require("@/assets/forge/logo.png"),
+        badge: require("@/assets/forge/badge.png"),
+    },
+};
