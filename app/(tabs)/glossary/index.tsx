92b2abebeb27f672ba2fda13d7fc2f5d20ccb7af
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-18 22:24:35 -0500

    Auto-save: 2025-09-18 22:24:35

--- app/(tabs)/glossary/index.tsx
+++ app/(tabs)/glossary/index.tsx
@@ -1,5 +1,5 @@
 // app/(tabs)/glossary/index.tsx
-
+im
 import React, { useState, useEffect } from 'react';
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { useCert } from '@/context/CertContext';
