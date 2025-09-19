0ded121911d87a3adb8d07a8c0fa65301dcc28e8
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-18 20:04:50 -0500

    Auto-save: 2025-09-18 20:04:50

--- app/(tabs)/course/index.tsx
+++ app/(tabs)/course/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'expo-router';
 import { useCert } from '@/context/CertContext';
 import { CERTS_ROADMAP } from '@/constants/certs';
 import {useTheme} from "@/context/ThemeContext";
-
+import ThemedButton from "@/components/ThemedButton";
 
 export default function CourseScreen() {
     const router = useRouter();
