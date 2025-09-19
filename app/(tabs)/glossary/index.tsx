a07ebd81c5735038a9b588ead59d40b2620a1c38
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-18 22:29:33 -0500

    Auto-save: 2025-09-18 22:29:33

--- app/(tabs)/glossary/index.tsx
+++ app/(tabs)/glossary/index.tsx
@@ -63,7 +63,7 @@ export default function GlossaryScreen() {
                         padding: 10,
                         backgroundColor: theme.colors.cardBackground,
                         borderRadius: 8,
-                        marginHorizontal: theme,
+                        marginHorizontal: theme.,
                     }}
                     onPress={() => {
                         setTab(t as any);
