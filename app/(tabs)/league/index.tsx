8b0a3853d532bd62ae735b634bb5cb80889ef30a
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-19 12:38:54 -0500

    Auto-save: 2025-09-19 12:38:54

--- app/(tabs)/league/index.tsx
+++ app/(tabs)/league/index.tsx
@@ -1,5 +1,7 @@
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
+import { useTheme } from '@/context/ThemeContext';
+
 
 // Forge-themed leagues (10 total) with individual colors
 const LEAGUES = [
