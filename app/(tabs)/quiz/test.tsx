b499262de44f2ee43378e015465d83a0b893bd81
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-19 23:15:00 -0500

    Auto-save: 2025-09-19 23:15:00

---
+++ app/(tabs)/quiz/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function TestQuiz() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Quiz Test Page Works!</Text>
+        </View>
+    );
+}
