9f3e9cc9e63b8c4899957d685aeccfd6fae036c8
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-20 19:28:08 -0500

    Auto-save: 2025-09-20 19:28:08

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -14,7 +14,7 @@ import { useRouter } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useEffect, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useAuth } from "@/context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import { useTheme } from "@/context/ThemeContext";
 import { Stack } from 'expo-router';
 
