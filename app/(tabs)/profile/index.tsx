54e2838f38aa0689d884b36eb402f6974e20f7d2
elijah <elijah.skinner66@gmail.com>
2025-09-22 16:02:11 -0500

    Auto-save: 2025-09-22 16:02:11

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState, useEffect } from 'react';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 import {useTheme} from "../../context/ThemeContext";
 
 export default function ProfileScreen() {
