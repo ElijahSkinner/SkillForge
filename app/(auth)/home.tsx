0969d6ef2e015de6cdf21022640ddd53be75b573
elijah <elijah.skinner66@gmail.com>
2025-09-23 14:03:20 -0500

    Auto-save: 2025-09-23 14:03:20

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -362,7 +362,7 @@ export default function RootHomeScreen() {
 
             <View style={styles.switchModeContainer}>
                 <Text style={styles.switchModeText}>
-                    Don't have an account?{' '}
+                    Dont have an account?{' '}
                 </Text>
                 <TouchableOpacity onPress={() => switchMode('register')}>
                     <Text style={styles.link}>
