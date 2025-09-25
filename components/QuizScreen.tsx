5f10a0601bc024326f35863e33295f0bfe0a4935
elijah <elijah.skinner66@gmail.com>
2025-09-25 16:53:38 -0500

    Auto-save: 2025-09-25 16:53:38

--- components/QuizScreen.tsx
+++ components/QuizScreen.tsx
@@ -169,7 +169,6 @@ export default function QuizScreen() {
                         disabled={!showResult}
                     />
                 </View>
-            />
             </SafeAreaView>
         </ThemedView>
     );
