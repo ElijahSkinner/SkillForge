94dfb4744b4b5ee5444b036286b03d8052255f99
Elijah Skinner <Elijahskinner66@gmail.com>
2025-09-25 19:24:13 -0500

    Auto-save: 2025-09-25 19:24:13

--- components/themed/index.ts
+++ components/themed/index.ts
@@ -4,3 +4,7 @@ export { ThemedText } from './ThemedText';
 export { ThemedButton } from './ThemedButton';
 export { ThemedSwitch } from './ThemedSwitch';
 export { ThemedModal } from './ThemedModal';
+export { ThemedCard } from './ThemedCard';
+export { ThemedBadge } from './ThemedBadge';
+export { ThemedProgressBar } from './ThemedProgressBar';
+export { ThemedIconButton } from './ThemedIconButton';
