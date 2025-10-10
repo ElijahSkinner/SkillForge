7e4cbeba895a3af41f79d7fb7ea87a0ad87af3f9
Elijah Skinner <Elijahskinner66@gmail.com>
2025-10-10 10:14:57 -0500

    Auto-save: 2025-10-10 10:14:57

--- components/modals/index.ts
+++ components/modals/index.ts
@@ -1,7 +1,8 @@
-// components/modals/index.ts - Updated with QuizExitModal
+// components/modals/index.ts
 export { ThemeSelectionModal } from './ThemeSelectionModal';
 export { AccountInfoModal } from './AccountInfoModal';
 export { ChangeEmailModal } from './ChangeEmailModal';
 export { ChangePasswordModal } from './ChangePasswordModal';
 export { LogoutConfirmModal } from './LogoutConfirmModal';
-export { default as QuizExitModal } from './QuizExitModal';
+export { QuizExitModal } from './QuizExitModal';
+export { ResetProgressModal } from './ResetProgressModal';  // ADD THIS
