fe8f2fc2906f5b32198c5ef811b2b068b641be48
Elijah Skinner <Elijahskinner66@gmail.com>
2025-10-10 09:38:53 -0500

    Auto-save: 2025-10-10 09:38:53

--- components/QuizScreen.tsx
+++ components/QuizScreen.tsx
@@ -333,7 +333,7 @@ export default function QuizScreen() {
             </SafeAreaView>
 
             {/* Themed Exit Confirmation Modal */}
-            <ThemedModal visible={showExitModal} onClose={cancelExit}>
+            <QuizExitModal visible={showExitModal} onClose={cancelExit}>
                 <View style={{ alignItems: 'center' }}>
                     {/* Warning Icon */}
                     <View style={{
@@ -383,7 +383,7 @@ export default function QuizScreen() {
                         />
                     </View>
                 </View>
-            </ThemedModal>
+            </QuizExitModal>
         </ThemedView>
     );
 }
